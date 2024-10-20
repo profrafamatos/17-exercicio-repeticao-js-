d1d94d5f0281015b708e37674493cc1b82cc5c6f
profrafamatos <rcmm@cesar.school>
2024-10-20 13:59:05 -0300

    Arquivos necessários para o exercício

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+/**
+ * Implemente uma função somaNumeros que recebe um número inteiro como parâmetro 
+ * e calcula a soma dos números de 1 até esse número fornecido. Use um laço 'for' para 
+ * iterar sobre os números de 1 até o número fornecido e calcular a soma.
+ * 
+ * Sintaxe:
+      for (inicialização; condição; incremento) {
+        // Bloco de código
+      }
+ */
+
 function somaNumeros(n) {
     /**** Escreva o código abaixo 👇******/
   
-  
-  
+
     
     /**** Escreva o código abaixo 👇******/
   }
